f4171938b46e78db1ecbfda63062dc884a7b3a10
Gustavo Lima <gustavo.lima@nd.com.br>
2021-06-16 16:57:20 -0300

    Feat: 1. Header created. 2. Style applied to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { GlobalStyle } from './styles/global';
+import {Header} from './components/Header'
 
 
 function App() {
   return (
-    <div className="App">
-    <h1>teste</h1>
+    <>
+    <Header/>
     <GlobalStyle/>
-    </div>
+    </>
   );
 }
 
