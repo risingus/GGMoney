2db86ef5b3597da0cf4ebe3589b8135a2769cbf9
Gustavo <gustavo.lima@nd.com.br>
2021-06-17 16:06:16 -0300

    Feat: 1. Installed polished and react-modal. 2. Created and styled modal and form. 3. Created states for input values. 4. Formated money value and data for view. 5. Created fake api database value and post fake endpoint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
